a8eb84337380a54213cb07168beb543a81d34362
Ayush Priyam <ayushpriyam24@gmail.com>
2023-08-25 11:46:43 +0530

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,10 @@
+import CustomButtom from "./CustomButtom";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker  from "./FilePicker";
+import Tab from "./Tab"
+
+
+
+
+export{CustomButtom,AIPicker,ColorPicker,FilePicker,Tab };
